f897396c8917f146a0640bce7267b20ff1e52da0
MarcinLach1 <MarcinLach1@outlook.com>
2018-10-22 02:05:51 +0200

    add
    Adding new functionality shopping list
    
    Co-Authored-By: marcinlach1 <marcinlach1@users.noreply.github.com>

---
+++ src/components/AddItemComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="input-group">
+    <input type="text" @keyup.enter="addItem" v-model="newItem" placeholder="dodaj produkt" class="form-control">
+    <span class="input-group-btn">
+      <button @click="addItem" class="btn btn-default" type="button">Dodaj!</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: ""
+    };
+  },
+  methods: {
+    addItem() {
+      var text;
+
+      text = this.newItem.trim();
+      if (text) {
+        this.$emit("add", this.newItem);
+        this.newItem = "";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
